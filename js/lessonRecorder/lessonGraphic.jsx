84bb1690a6a33f1a58dbbea0d54f9c81360e42aa
HironoriOnishi <onishi.hironori@gmail.com>
2018-09-02 01:43:43 +0900

    adjusted graphic size in lesson recorder

--- js/lessonRecorder/lessonGraphic.jsx
+++ js/lessonRecorder/lessonGraphic.jsx
@@ -2,30 +2,25 @@ import React from 'react';
 // TODO add spinner until loaded image.
 export default ({url}) => (
     <div id="lesson-graphic">
-        <div id="lesson-graphic-cell">
-            <img src={url} id="lesson-graphic-body" />
-        </div>
+        <img src={url} id="lesson-graphic-body" />
         <style jsx>{`
             #lesson-graphic {
                 position: absolute;
                 top: 0;
-                width: 100%;
-                height :100%;
-                display: table;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                width: 90%;
+                height: 90%;
+                margin: auto;
                 z-index: -100;
-            }
-            #lesson-graphic-cell {
-                position: relative;
-                display: table-cell;
-                text-align: center;
-                vertical-align: center;
             }
             #lesson-graphic-body {
                 display: ${url != '' ? 'block' : 'none'};
-                width: 90%;
-                max-height: 90%;
+                width: 100%;
+                height: 100%;
                 object-fit: contain;
-                animation-duration: 0.1s;
+                animation-duration: 0.3s;
                 animation-name: ${'fade-in'};
             }
         `}</style>
