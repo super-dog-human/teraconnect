396e4d0a8aa42de42ebcd0d28e9bb0bffaa2ae0e
SuperDogHuman <super.dog.less.than.human@gmail.com>
2018-11-15 12:42:31 +0900

    apply linter to footer component

--- js/footer/index.js
+++ js/footer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 export default () => (
     <footer className="fixed-bottom">
@@ -6,25 +6,25 @@ export default () => (
             <a href="https://goo.gl/forms/Rmp3dNKN7ZsDoF2k2">ZYGOPTERA</a>
         </div>
         <style jsx>{`
-        footer > div {
-            position: relative;
-            width: 100%;
-            height: 50px;
-        }
-        footer > div > a {
-            position: absolute;
-            color: var(--soft-white);
-            font-size: 14px;
-            height: 20px;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            margin: auto;
-        }
-        footer > div > a:hover {
-            text-decoration: none;
-        }
+            footer > div {
+                position: relative;
+                width: 100%;
+                height: 50px;
+            }
+            footer > div > a {
+                position: absolute;
+                color: var(--soft-white);
+                font-size: 14px;
+                height: 20px;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                margin: auto;
+            }
+            footer > div > a:hover {
+                text-decoration: none;
+            }
         `}</style>
     </footer>
 )
