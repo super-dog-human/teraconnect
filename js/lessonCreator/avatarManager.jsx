f1317f9d35fab8144efe5ce485c2b9f24251b8cc
SuperDogHuman <super.dog.less.than.human@gmail.com>
2018-11-17 16:06:29 +0900

    move js class file into utils direcotory

--- js/lessonCreator/avatarManager.jsx
+++ js/lessonCreator/avatarManager.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Dropzone from 'react-dropzone'
-import AvatarRightsChecker from './avatarRightsChecker'
+import AvatarRightsChecker from './utils/avatarRightsChecker'
 import { fetchAvatars, uploadAvatar } from '../common/networkManager'
 
 export default class AvatarManager extends React.Component {
--- js/lessonCreator/utils/avatarRightsChecker.js
+++ js/lessonCreator/utils/avatarRightsChecker.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import '../common/GLTFLoader'
+import '../../common/GLTFLoader'
 
 export default class AvatarRightsChecker {
     constructor() {
