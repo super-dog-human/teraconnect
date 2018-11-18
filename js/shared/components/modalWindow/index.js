db9014b42e8ec710d62dc2133e2f17f6873c24d9
SuperDogHuman <super.dog.less.than.human@gmail.com>
2018-11-18 21:56:36 +0900

    fix modal to don't close when clicking modal

--- js/shared/components/modalWindow/index.js
+++ js/shared/components/modalWindow/index.js
@@ -1,112 +1,131 @@
 import React from 'react'
-import styled from '@emotion/styled'
 import { css } from 'emotion'
 
 export default props => (
     <Modal
         isOpen={props.isOpen}
-        closeCallback={() => {
-            console.log('back')
-            // if (needsConfirm) return
-            this.closeModal()
-        }}
+        onClose={props.onClose}
+        needsConfirm={props.needsConfirm}
     >
-        <ModalHeader title={props.title} isError={props.isError} />
-        <ModalBody message={props.message} />
+        <ModalHeader {...props} />
+        <ModalBody message={props.message} onClick={e => e.stopPropagation()} />
         <ModalFooter {...props} />
     </Modal>
 )
 
-const modalStyle = {}
+const Modal = ({ isOpen, onClose, needsConfirm, children }) => {
+    const modalStyle = css`
+        display: block;
+        background-color: rgba(255, 255, 255, 0.4);
+        color: var(--dark-gray);
+    `
 
-const Modal = ({ isOpen, closeCallback, children }) => (
-    <div
-        className={isOpen ? 'modal fade show' : 'modal fade show'}
-        style={{
-            //            display: isOpen ? 'block' : 'none',
-            display: 'none',
-            backgroundColor: 'rgba(255, 255, 255, 0.4)',
-            color: 'var(--dark-gray)'
-        }}
-        tabIndex="-1"
-        role="dialog"
-        onClick={() => {
-            console.log('modal clicked')
-        }}
-        //{close()
-    >
-        <div className="modal-dialog" role="document">
-            <div className="modal-content">{children}</div>
+    return (
+        <div
+            className={`${modalStyle} ${
+                isOpen ? 'modal fade show' : 'modal fade'
+            }`}
+            tabIndex="-1"
+            role="dialog"
+            onClick={() => {
+                if (needsConfirm) return
+                onClose()
+            }}
+        >
+            <div className="modal-dialog" role="document">
+                <div
+                    className="modal-content"
+                    onClick={e => {
+                        e.stopPropagation() // don't close modal when clicking content
+                    }}
+                >
+                    {children}
+                </div>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
-const ModalHeader = ({ title, isError }) => (
-    // isError
-    <div className="modal-header">
-        <h5 className="modal-title">{title}</h5>
+const ModalHeader = props => {
+    const ModalCrossButton = ({ onClose }) => (
         <button
             type="button"
             className="close"
             data-dismiss="modal"
             onClick={() => {
-                console.log('closs clicked')
+                onClose()
             }}
         >
             <span aria-hidden="true">&times;</span>
         </button>
-    </div>
-)
+    )
+
+    return (
+        <div className="modal-header">
+            <h5
+                className={
+                    props.isError ? 'modal-title text-danger' : 'modal-title'
+                }
+            >
+                {props.title}
+            </h5>
+            {props.needsConfirm ? (
+                ''
+            ) : (
+                <ModalCrossButton onClose={props.onClose} />
+            )}
+        </div>
+    )
+}
 
 const ModalBody = props => <div className="modal-body">{props.message}</div>
 
-const ModalFooter = props => (
-    <div className="modal-footer">
-        <ModalFooterButton {...props} />
-    </div>
-)
-
-const ModalFooterButton = props => {
-    if (props.needsConfirmModal) {
-        return (
-            <ModalConfirmButton
-                okCallback={props.modalOKCallback}
-                cancelCallback={props.modalCancelCallback}
-            />
-        )
-    } else {
-        return <ModalCloseButton callback={props.modalCloseCallback} />
+const ModalFooter = props => {
+    const ModalFooterButton = props => {
+        if (props.needsConfirm) {
+            return (
+                <ModalConfirmButton
+                    onClickCancel={() => props.onClickCancel()}
+                    onClickOK={() => {
+                        props.onClickOK()
+                    }}
+                />
+            )
+        } else {
+            return <ModalCloseButton onClose={props.onClose} />
+        }
     }
+
+    const ModalConfirmButton = ({ onClickCancel, onClickOK }) => (
+        <>
+            <ModalButton
+                type="secondary"
+                clickFunction={onClickCancel}
+                text="キャンセル"
+            />
+            <ModalButton type="primary" callback={onClickOK} text="OK" />
+        </>
+    )
+
+    const ModalCloseButton = ({ onClose }) => (
+        <ModalButton type="secondary" callback={onClose} text="閉じる" />
+    )
+
+    const ModalButton = ({ type, callback, text }) => (
+        <button
+            type="button"
+            className={`btn btn-${type}`}
+            onClick={() => {
+                callback()
+            }}
+        >
+            {text}
+        </button>
+    )
+
+    return (
+        <div className="modal-footer">
+            <ModalFooterButton {...props} />
+        </div>
+    )
 }
-
-const ModalConfirmButton = ({ okCancelCallback, cancelCallback }) => (
-    <>
-        <ModalButton
-            type="secondary"
-            clickFunction={cancelCallback}
-            text="キャンセル"
-        />
-        <ModalButton
-            type="primary"
-            clickFunction={okCancelCallback}
-            text="OK"
-        />
-    </>
-)
-
-const ModalCloseButton = ({ callback }) => (
-    <ModalButton type="secondary" clickFunction={callback} text="閉じる" />
-)
-
-const ModalButton = ({ type, clickFunction, text }) => (
-    <button
-        type="button"
-        className={`btn btn-${type}`}
-        onClick={() => {
-            clickFunction()
-            console.log('button clicked')
-        }}
-    >
-        {text}
-    </button>
-)
