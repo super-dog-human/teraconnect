443b98177e2f271847400f725d2a5844de3c4550
SuperDogHuman <super.dog.less.than.human@gmail.com>
2018-11-22 00:58:02 +0900

    replace styled-jsx in lesson graphics component  to emotion

--- js/shared/components/lessonController/lessonGraphics.jsx
+++ js/shared/components/lessonController/lessonGraphics.jsx
@@ -1,48 +1,44 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const LessonGraphics = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 0; // graphic
+`
+
+const LessonGraphicContainer = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+`
+
+const LessonGraphicSrc = styled.img`
+    width: ${props => `${props.sizePct > 0 ? props.sizePct : 100}%`};
+    height: ${props => `${props.sizePct > 0 ? props.sizePct : 100}%`};
+    object-fit: contain;
+    animation-duration: 0.3s;
+    animation-name: ${'fade-in'};
+`
 
 const LessonGraphic = ({ graphic }) => (
-    <div className="lesson-graphic">
-        <img src={graphic.url} id="lesson-graphic-body" />
-        <style jsx>{`
-            .lesson-graphic {
-                position: absolute;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                width: 100%;
-                height: 100%;
-                margin: auto;
-            }
-            #lesson-graphic-body {
-                width: ${graphic.sizePct || 100}%;
-                height: ${graphic.sizePct || 100}%;
-                object-fit: contain;
-                animation-duration: 0.3s;
-                animation-name: ${'fade-in'};
-            }
-        `}</style>
-    </div>
+    <LessonGraphicContainer>
+        <LessonGraphicSrc
+            src={graphic.url}
+            width={graphic.sizePct}
+            height={graphic.sizePct}
+        />
+    </LessonGraphicContainer>
 )
 
 export default ({ graphics }) => (
-    <div id="lesson-graphics">
+    <LessonGraphics>
         {graphics.map((graphic, i) => {
-            return (
-                <LessonGraphic
-                    graphic={graphic}
-                    key={i}
-                    className="lesson-graphic"
-                />
-            )
+            return <LessonGraphic graphic={graphic} key={i} />
         })}
-        <style jsx>{`
-            #lesson-graphics {
-                position: absolute;
-                width: 100%;
-                height: 100%;
-                z-index: 0; // graphic
-            }
-        `}</style>
-    </div>
+    </LessonGraphics>
 )
