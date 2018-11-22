52e02cd9ffddf374b9434db9a3e5b5b88220bda8
SuperDogHuman <super.dog.less.than.human@gmail.com>
2018-11-23 00:41:28 +0900

    add range slider component

---
+++ js/shared/components/rangeSlider/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+export default props => (
+    <RangeSlider
+        type="range"
+        value="0"
+        min="0"
+        step="any"
+        {...props}
+        onMouseUp={event => {
+            event.target.blur()
+        }}
+    />
+)
+
+const RangeSlider = styled.input`
+    appearance: none;
+    -webkit-appearance: none;
+    position: absolute;
+    bottom: 7%;
+    left: 3%;
+    right: 3%;
+    width: 94%;
+    height: 4px;
+    background-color: rgba(0, 0, 0, 0.2);
+    z-index: 250; // seek bar
+    cursor: pointer;
+    $:hover {
+        opacity: 0.7;
+    }
+    &:focus,
+    &:active {
+        outline: none;
+    }
+    :-moz-focusring {
+        outline: none;
+    }
+    &::-webkit-slider-thumb {
+        appearance: none;
+        -webkit-appearance: none;
+        position: relative;
+        width: 15px;
+        height: 15px;
+        display: block;
+        background-color: var(--dark-gray);
+        border-radius: 50%;
+        -webkit-border-radius: 50%;
+    }
+    &::-moz-range-thumb {
+        appearance: none;
+        -webkit-appearance: none;
+        position: relative;
+        width: 15px;
+        height: 15px;
+        display: block;
+        background-color: var(--dark-gray);
+        border-radius: 50%;
+        -webkit-border-radius: 50%;
+    }
+`
