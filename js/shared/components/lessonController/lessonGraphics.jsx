41fe8bd198e04638b402aca005feafdbcf689555
SuperDogHuman <super.dog.less.than.human@gmail.com>
2018-11-22 13:52:12 +0900

    fix graphic layout in lesson controller

--- js/shared/components/lessonController/lessonGraphics.jsx
+++ js/shared/components/lessonController/lessonGraphics.jsx
@@ -1,44 +1,52 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-const LessonGraphics = styled.div`
+const LessonGraphicContainer = styled.div`
     position: absolute;
+    display: table;
     width: 100%;
     height: 100%;
     z-index: 0; // graphic
-`
-
-const LessonGraphicContainer = styled.div`
-    position: absolute;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
-    margin: auto;
 `
 
-const LessonGraphicSrc = styled.img`
-    width: ${props => `${props.sizePct > 0 ? props.sizePct : 100}%`};
-    height: ${props => `${props.sizePct > 0 ? props.sizePct : 100}%`};
+const LessonGraphicCell = styled.div`
+    display: table-cell;
+    text-align: ${props =>
+        props.horizontalAlign != '' ? props.horizontalAlign : 'center'};
+    vertical-align: ${props =>
+        props.verticalAlign != '' ? props.verticalAlign : 'middle'};
+`
+
+const LessonGraphic = styled.img`
+    max-width: 100%;
+    max-height: 100%;
     object-fit: contain;
     animation-duration: 0.3s;
-    animation-name: ${'fade-in'};
+    animation-name: fadeIn;
 `
 
-const LessonGraphic = ({ graphic }) => (
-    <LessonGraphicContainer>
-        <LessonGraphicSrc
-            src={graphic.url}
-            width={graphic.sizePct}
-            height={graphic.sizePct}
-        />
-    </LessonGraphicContainer>
-)
-
 export default ({ graphics }) => (
-    <LessonGraphics>
+    <LessonGraphicContainer>
         {graphics.map((graphic, i) => {
-            return <LessonGraphic graphic={graphic} key={i} />
+            const graphicWidthPercent =
+                graphic.sizePct > 0 ? graphic.sizePct : 100
+            return (
+                <LessonGraphicCell
+                    key={i}
+                    horizontalAlign={graphic.horizontalAlign}
+                    verticalAlign={graphic.verticalAlign}
+                >
+                    <LessonGraphic
+                        src={graphic.url}
+                        width={`${graphicWidthPercent}%`}
+                        // TODO support height order
+                    />
+                </LessonGraphicCell>
+            )
         })}
-    </LessonGraphics>
+    </LessonGraphicContainer>
 )
