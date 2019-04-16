9be23aa55378ee13ef2608c78008c6750e39139a
SuperDogHuman <super.dog.less.than.human@gmail.com>
2019-04-16 22:28:21 +0900

    add context file for lesson creator

---
+++ js/lessonCreator/context.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LessonIDContext = React.createContext(null)
+export const AvatarIDContext = React.createContext(null)
+export const GraphicIDsContext = React.createContext(null)
+export const TitleContext = React.createContext(null)
+export const DescriptionContext = React.createContext(null)
