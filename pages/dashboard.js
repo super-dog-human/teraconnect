c5d92fbac252c1073d5150ed4440cedc3a22465b
SuperDog <super.dog.less.than.human@gmail.com>
2020-11-10 18:09:05 +0900

    add dashboard page

---
+++ pages/dashboard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Head from 'next/head'
+import Layout from '../components/layout'
+import requirePageAuth from '../components/requirePageAuth'
+import { signOut } from 'next-auth/client'
+
+const Page = () => {
+  return (
+    <>
+      <Head>
+        <title>TERACONNECT</title>
+      </Head>
+      <Layout>
+        <>
+          <button className="light" onClick={() => signOut()}>ログアウト</button>
+        </>
+      </Layout>
+    </>
+  )
+}
+
+export default Page
+
+export async function getServerSideProps(context) {
+  return await requirePageAuth(context)
+}
