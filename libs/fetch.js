1b1819f4b2560851bc59b59f89c50501f13e86db
SuperDog <super.dog.less.than.human@gmail.com>
2020-12-25 11:02:03 +0900

    add methods for GET and POST using isomorphic-unfetch

--- libs/fetch.js
+++ libs/fetch.js
@@ -1,7 +1,30 @@
-import fetch from 'isomorphic-unfetch'
+import { default as isoFetch } from 'isomorphic-unfetch'
 
-export default async function Fetch(...args) {
-  args[0] = process.env.TERACONNECT_API_URL + args[0]
-  const res = await fetch(...args)
-  return await res.json()
+export async function fetch(resource, option) {
+  const url = process.env.TERACONNECT_API_URL + resource
+  const response = await isoFetch(url, option)
+
+  if (response.ok) {
+    return await response.json()
+  }
+
+  const error = new Error(response.statusText)
+  error.response = response
+  throw error
+}
+
+export function fetchWithAuth(resource, token) {
+  return fetch(resource, { headers: headerWithToken(token) })
+}
+
+export function post(resource, body, token) {
+  return fetch(resource, {
+    method: 'POST',
+    headers: headerWithToken(token),
+    body: JSON.stringify(body)
+  })
+}
+
+function headerWithToken(token) {
+  return  { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` }
 }
