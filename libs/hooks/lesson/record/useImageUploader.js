40ccbdd8d815017a315c4e89a7c6cd9185904746
SuperDog <super.dog.less.than.human@gmail.com>
2021-01-21 12:16:42 +0900

    スライド画像のサムネイルは元画像をcanvasで小さくリサイズしたものを使用する

--- libs/hooks/lesson/record/useImageUploader.js
+++ libs/hooks/lesson/record/useImageUploader.js
@@ -22,12 +22,31 @@ export default function useImageUploader(id, token, setImages, inputFileRef) {
       const reader = new FileReader()
       reader.readAsDataURL(file)
       reader.onload = (e => {
-        setImages(images =>
-          // canvasでリサイズしたものを渡す
-          [...images, { src: e.target.result, id: Math.random().toString(32).substring(2), isUploading: true }]
-        )
+        resizedImageDataURL(e.target.result, (imageDataURL) => {
+          setImages(images =>
+            [...images, { src: e.target.result, thumbnail: imageDataURL, id: Math.random().toString(32).substring(2), isUploading: true }]
+          )
+        })
       })
       //      uploadImage(file, i)
+    })
+  }
+
+  function resizedImageDataURL(original, callback) {
+    const image = new Image()
+    image.src = original
+    image.onload = (() => {
+      const ratio = Math.min(150.0 / image.naturalWidth, 95.0 / image.naturalHeight) * window.devicePixelRatio
+      const canvas = document.createElement('canvas')
+      const ctx = canvas.getContext('2d')
+
+      const width = image.naturalWidth * ratio
+      const height = image.naturalHeight * ratio
+
+      canvas.width = width
+      canvas.height = height
+      ctx.drawImage(image, 0, 0, width, height)
+      callback(canvas.toDataURL())
     })
   }
 
