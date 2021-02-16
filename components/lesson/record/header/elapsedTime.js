5d9b8beda90d698f7c92dc93d5b25b091aa81841
SuperDog <super.dog.less.than.human@gmail.com>
2021-02-16 16:01:20 +0900

    軽微な修正

--- components/lesson/record/header/elapsedTime.js
+++ components/lesson/record/header/elapsedTime.js
@@ -1,14 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import React, { useState, useEffect } from 'react'
 import { css } from '@emotion/core'
+import { floatSecondsToMinutesFormat } from '../../../../libs/utils'
 
 export default function ElapsedTime({ elapsedSeconds, className }) {
   const [displayTime, setDisplayTime] = useState('')
 
   useEffect(() => {
-    const minutes = Math.floor(elapsedSeconds / 60) % 60
-    const seconds = Math.floor(elapsedSeconds - minutes * 60)
-    setDisplayTime(`${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`)
+    setDisplayTime(floatSecondsToMinutesFormat(elapsedSeconds))
   }, [elapsedSeconds])
 
   return (
