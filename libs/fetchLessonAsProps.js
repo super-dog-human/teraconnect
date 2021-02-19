df6dfcaec98322a277f24081b08de0602dde5911
SuperDog <super.dog.less.than.human@gmail.com>
2021-02-19 21:21:38 +0900

    サーバーサイドでLessonを取得してpropsで返す関数を追加

---
+++ libs/fetchLessonAsProps.js
@@ -0,0 +1,15 @@
+import { fetchWithAuth } from './fetch'
+
+export default async function fetchLessonAsProps(context, props) {
+  const id = context.query.id
+  return fetchWithAuth(`/lessons/${id}?for_authoring=true`, props.token)
+    .then(lesson => ({ props: { ...props, lesson }}))
+    .catch(e => {
+    if (e.response?.status === '401') {
+      context.res.writeHead(307, { Location: '/login' })
+      context.res.end()
+    } else {
+      throw e
+    }
+  })
+}
