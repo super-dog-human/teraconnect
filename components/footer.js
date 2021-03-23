0114edb895d7a6b3f7000edd2a7367fb7d479ffb
SuperDog <super.dog.less.than.human@gmail.com>
2021-03-23 21:02:48 +0900

    フッターのレイアウト修正

--- components/footer.js
+++ components/footer.js
@@ -3,70 +3,69 @@ import React from 'react'
 import Link from 'next/link'
 import { useSession } from 'next-auth/client'
 import { css } from '@emotion/core'
-import { Container, Row, Col, Hidden } from 'react-grid-system'
+import { useScreenClass, Container, Row, Col } from 'react-grid-system'
 
 export default function Footer() {
   const [ session, loading ] = useSession()
+  const screenClass = useScreenClass()
+
+  const backgroundStyle = css({
+    width: '100%',
+    minHeight: '140px',
+    backgroundColor: 'var(--dark-gray)',
+    paddingTop: '30px',
+    paddingBottom: '30px',
+    textAlign: ['xs', 'sm'].includes(screenClass) ? 'center' : 'left',
+  })
 
   return (
-    <footer className="text-align-center" css={bodyStyle}>
-
-      <div css={logoStyle}>
-        {!loading && !session && <div>
-          <div css={logoCopyStyle}>君の光をさがそう。</div>
-          <img src="/img/logo_text.png" />
-        </div>
-        }
-
-        {!loading && session && <div>
-          <div css={logoCopyStyle}>あなたの知識が、誰かを照らす。</div>
-          <img src="/img/logo_text.png" />
-        </div>
-        }
-      </div>
-
-      <Container fluid>
-        <Row justify="center" css={footerMenus}>
-          <Col md={3}>
-            <div>
-              <Link href="/">サービス概要</Link>
+    <footer className="text-align-center" css={backgroundStyle}>
+      <Container fluid css={bodyStyle}>
+        <Row justify="center" css={rowStyle}>
+          <Col md={4} css={logoContainerStyle}>
+            {!loading && !session && <div>
+              <div css={logoCopyStyle}>君の光をさがそう。</div>
+              <img src="/img/logo.png" srcSet="/img/logo.png 1x, /img/logo@2x.png 2x" />
             </div>
-            <Hidden xs sm>&nbsp;</Hidden>
-            <div>
+            }
+
+            {!loading && session && <div>
+              <div css={logoCopyStyle}>あなたの知識が、誰かを照らす。</div>
+              <img src="/img/logo.png" srcSet="/img/logo.png 1x, /img/logo@2x.png 2x" />
+            </div>
+            }
+          </Col>
+          <Col md={2}>
+            <div css={categoryStyle}>使いかた</div>
+            <div css={linkStyle}>
+              <Link href="/">授業をさがす</Link>
+            </div>
+            <div css={linkStyle}>
+              <Link href="/">授業をつくる</Link>
+            </div>
+            <div css={linkStyle}>
+              <Link href="/">よくある質問</Link>
+            </div>
+          </Col>
+          <Col md={2}>
+            <div css={categoryStyle}>サービスについて</div>
+            <div css={linkStyle}>
+              <Link href="/">TERACONNECTとは</Link>
+            </div>
+            <div css={linkStyle}>
               <Link href="/">ライセンス表記</Link>
             </div>
-            <Hidden xs sm>&nbsp;</Hidden>
-            <div>
+            <div css={linkStyle}>
               <Link href="/">運営者</Link>
             </div>
           </Col>
-
-          <Col md={3}>
-            <div>
-              <Link href="/">はじめ方</Link>
-            </div>
-            <Hidden xs sm>&nbsp;</Hidden>
-            <div>
-              <Link href="/">授業をさがす</Link>
-            </div>
-            <Hidden xs sm>&nbsp;</Hidden>
-            <div>
-              <Link href="/">授業をつくる</Link>
-            </div>
-            <Hidden xs sm>&nbsp;</Hidden>
-          </Col>
-
-          <Col md={3}>
-            <div>
-              <Link href="/">よくある質問</Link>
-            </div>
-            <Hidden xs sm>&nbsp;</Hidden>
-            <div>
+          <Col md={2}>
+            <div css={categoryStyle}>ご利用にあたって</div>
+            <div css={linkStyle}>
               <Link href="/terms_of_use">利用規約</Link>
             </div>
-            <Hidden xs sm>&nbsp;</Hidden>
-            <div>
-              <a href="https://goo.gl/forms/Rmp3dNKN7ZsDoF2k2">問い合わせ</a>
+            <div css={linkStyle}>
+              <a href="https://goo.gl/forms/Rmp3dNKN7ZsDoF2k2">お問い合わせ</a>
             </div>
           </Col>
         </Row>
@@ -76,32 +75,46 @@ export default function Footer() {
 }
 
 const bodyStyle = css({
-  width: '100%',
-  paddingTop: '120px',
-  paddingBottom: '120px',
+  maxWidth: '1280px',
+  height: '100%',
+  display: 'flex',
   justifyContent: 'center',
-  backgroundColor: 'var(--dark-gray)',
-  color: 'var(--text-gray)',
+  alignItems: 'center',
   a: {
     color: 'var(--text-gray)',
   },
   'a:hover': {
-    opacity: 0.8,
+    filter: 'brightness(130%)',
   },
 })
 
-const logoStyle = css({
-  height: '63px',
-  marginBottom: '80px',
+const rowStyle = css({
+  width: '100%',
+})
+
+const logoContainerStyle = css({
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
 })
 
 const logoCopyStyle = css({
-  fontSize: '12px',
+  color: 'var(--text-gray)',
+  fontSize: '13px',
   marginBottom: '15px',
 })
 
-const footerMenus = css({
-  div: {
-    minHeight: '30px',
-  }
+const categoryStyle = css({
+  color: 'var(--text-gray)',
+  filter: 'brightness(130%)',
+  fontSize: '16px',
+  fontWeight: 'bold',
+  marginTop: '15px',
+  marginBottom: '15px',
+})
+
+const linkStyle = css({
+  fontSize: '14px',
+  marginTop: '5px',
+  marginBottom: '5px',
 })
