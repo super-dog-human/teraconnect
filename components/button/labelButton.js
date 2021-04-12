31806568b9ee4cd031d202610a7fb755612e879e
SuperDog <super.dog.less.than.human@gmail.com>
2021-04-12 16:19:19 +0900

    文言表示ボタンに無効状態のpropsを追加

--- components/button/labelButton.js
+++ components/button/labelButton.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { css } from '@emotion/core'
 
-export default function LabelButton({ children, size, backgroundColor='inherit', color='inherit', borderColor='inherit', onClick }) {
+export default function LabelButton({ children, size, backgroundColor='inherit', color='inherit', borderColor='inherit', onClick, disabled=false }) {
   const bodyStyle = css({
     width: '100%',
     height: '100%',
@@ -16,7 +16,7 @@ export default function LabelButton({ children, size, backgroundColor='inherit',
   })
 
   return (
-    <button onClick={onClick} css={bodyStyle}>
+    <button onClick={onClick} css={bodyStyle} disabled={disabled}>
       {children}
     </button>
   )
