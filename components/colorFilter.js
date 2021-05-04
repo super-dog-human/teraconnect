5c782cfc9181ff9eb7a4112a4ea4f7d80975dabb
SuperDog <super.dog.less.than.human@gmail.com>
2021-05-04 20:12:52 +0900

    filter変更用の汎用コンポーネントをhover時に別の設定を受け付け可能にする

--- components/colorFilter.js
+++ components/colorFilter.js
@@ -2,9 +2,12 @@
 import React from 'react'
 import { css } from '@emotion/core'
 
-export default function ColorFilter({ children, filter, display }) {
+export default function ColorFilter({ children, filter, hoverFilter, display }) {
   const bodyStyle = css({
     filter,
+    ':hover': {
+      filter: hoverFilter,
+    },
     display,
   })
 
