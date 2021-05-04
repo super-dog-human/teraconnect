3ae2198685c83877858702b5de1fbdc0fef73f60
SuperDog <super.dog.less.than.human@gmail.com>
2021-05-04 20:13:32 +0900

    汎用コンポーネントにvisibility設定用のpropsを追加

--- components/container.js
+++ components/container.js
@@ -2,15 +2,17 @@
 import React from 'react'
 import { css } from '@emotion/core'
 
-export default function Container({ children, width, height, minWidth, minHeight, maxWidth, maxHeight, display='block' }) {
+export default function Container({ children, width, height, minWidth, minHeight, maxWidth, maxHeight, display, invisible, position }) {
   const bodyStyle = css({
+    display,
+    visibility: invisible ? 'hidden' : 'visible',
+    position,
     width: width ? `${width}px` : 'auto',
     height: height ? `${height}px` : 'auto',
     minWidth: minWidth ? `${minWidth}px` : 'auto',
     minHeight: minHeight ? `${minHeight}px` : 'auto',
     maxWidth: maxWidth ? `${maxWidth}px` : 'none',
     maxHeight: maxHeight ? `${maxHeight}px` : 'none',
-    display,
   })
 
   return (
