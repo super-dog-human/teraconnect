a3d0a902643580e6175afd687a813c92d9f30ebe
SuperDog <super.dog.less.than.human@gmail.com>
2021-05-17 16:37:46 +0900

    filterを適用する汎用コンポーネントにsafari対応を追加

--- components/colorFilter.js
+++ components/colorFilter.js
@@ -5,6 +5,7 @@ import { css } from '@emotion/core'
 export default function ColorFilter({ children, filter, hoverFilter }) {
   const bodyStyle = css({
     filter,
+    transform: 'translateZ(0)', // safariの描画ゴミ抑止のためGPUを使用する
     ':hover': {
       filter: hoverFilter,
     },
