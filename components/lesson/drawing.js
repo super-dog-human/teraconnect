3e170b823f96bb8e5d9146e05269955b31b0389f
SuperDog <super.dog.less.than.human@gmail.com>
2021-05-18 09:53:40 +0900

    フリーハンド描写の表示がオフでも背景色は表示し続ける

--- components/lesson/drawing.js
+++ components/lesson/drawing.js
@@ -3,19 +3,24 @@ import React from 'react'
 import { css } from '@emotion/core'
 
 export default function LessonDrawing({ drawingRef, isHide, startDrawing, inDrawing, endDrawing, backgroundColor, zKind }) {
+  const backgroundStyle = css({
+    backgroundColor: backgroundColor,
+    position: 'absolute',
+    top: 0,
+  })
+
   const bodyStyle = css({
     visibility: isHide ? 'hidden' : 'visible',
-    position: 'absolute',
-    cursor: 'pointer',
-    top: 0,
     width: '100%',
     height: '100%',
-    backgroundColor: backgroundColor,
+    cursor: 'pointer',
   })
 
   return (
-    <canvas width='1280' height='720' css={bodyStyle} ref={drawingRef}
-      onMouseDown={startDrawing} onMouseMove={inDrawing} onMouseUp={endDrawing} onMouseLeave={endDrawing}
-      onTouchStart={startDrawing} onTouchMove={inDrawing} onTouchEnd={endDrawing} onTouchCancel={endDrawing} className={zKind && `${zKind}-z`} />
+    <div css={backgroundStyle} className={zKind && `${zKind}-z`}>
+      <canvas width='1280' height='720' css={bodyStyle} ref={drawingRef}
+        onMouseDown={startDrawing} onMouseMove={inDrawing} onMouseUp={endDrawing} onMouseLeave={endDrawing}
+        onTouchStart={startDrawing} onTouchMove={inDrawing} onTouchEnd={endDrawing} onTouchCancel={endDrawing} />
+    </div>
   )
 }
