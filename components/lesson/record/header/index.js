c9d8b0e136a07eecdaaf705697989fcb2ec507ec
SuperDog <super.dog.less.than.human@gmail.com>
2021-05-20 16:14:50 +0900

    軽微な修正

--- components/lesson/record/header/index.js
+++ components/lesson/record/header/index.js
@@ -8,12 +8,18 @@ import TopLogoLink from '../../../topLogoLink'
 import RecordingButton from './recordingButton'
 import DrawingConfigPanel from '../drawingController/drawingConfigPanel'
 import DrawingConfigButton from '../drawingController/drawingConfigButton'
-import useDrawingController from '../../../../libs/hooks/lesson/useDrawingController'
 
 export default function LessonRecordHeader({ lessonID, isMicReady, isDrawingHide, setIsDrawingHide, enablePen, setEnablePen,
   undoDrawing, clearDrawing, drawingColor, setDrawingColor, setDrawingLineWidth, isShowControlPanel, setIsShowControlPanel }) {
   const { isFinishing } = useLessonRecorderContext()
-  const { handleDrawingHide, handlePen } = useDrawingController({ setIsDrawingHide, setEnablePen })
+
+  function handleDrawingHide() {
+    setIsDrawingHide(state => !state)
+  }
+
+  function handlePen() {
+    setEnablePen(state => !state)
+  }
 
   function handleSettingPanel() {
     setIsShowControlPanel(state => !state)
