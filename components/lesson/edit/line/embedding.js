51573dbc56650e5f43e5a333fdb84e338acc21fc
SuperDog <super.dog.less.than.human@gmail.com>
2021-05-28 18:37:57 +0900

    埋め込み動画用のcomponentの土台とアイコン画像を追加

---
+++ components/lesson/edit/line/embedding.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import KindIcon from './kindIcon'
+import ActionLabel from './actionLabel'
+import EditIcon from './editIcon'
+
+export default function LessonLineEmbedding({ embedding, index, isEditButtonShow, handleEditClick }) {
+  function handleEditButtonClick(e) {
+    e.stopPropagation()
+    handleEditClick(e, 'embedding', index, embedding)
+  }
+
+  return (
+    <>
+      <KindIcon kind="embedding" />
+      <ActionLabel kind="embedding" />
+      <EditIcon isShow={isEditButtonShow} onClick={handleEditButtonClick} />
+    </>
+  )
+}
---
+++ components/lesson/edit/lineConfig/embedding/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Container from '../../../../container'
+import ContainerSpacer from '../../../../containerSpacer'
+import AlignContainer from '../../../../alignContainer'
+import IconButton from '../../../../button/iconButton'
+import DragHandler from '../../../../dragHandler'
+import DialogFooter from '../configDialog/dialogFooter'
+
+export default function Embedding(props) {
+  const tabConfig = { elapsedTime: 0 }
+  const isProcessing = false
+
+  function handleCancel() {
+    props.closeCallback(true)
+  }
+
+  function handleConfirm() {
+    props.closeCallback()
+  }
+
+  function setConfig() {
+
+  }
+
+  return (
+    <>
+      <DragHandler>
+        <AlignContainer textAlign='right'>
+          <Container width='36' height='36' display='inline-block'>
+            <IconButton name={'close'} padding='10' onClick={handleCancel} disabled={isProcessing} />
+          </Container>
+        </AlignContainer>
+      </DragHandler>
+      <Container height='60'>
+        <ContainerSpacer left='50' right='50'>
+          <DialogFooter elapsedTime={tabConfig.elapsedTime} setConfig={setConfig} onConfirm={handleConfirm} onCancel={handleCancel} isProcessing={isProcessing} />
+        </ContainerSpacer>
+      </Container>
+    </>
+  )
+}
