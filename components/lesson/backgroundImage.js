6d20688b010d103c8b92cc6fbaf57ac2f454f455
SuperDog <super.dog.less.than.human@gmail.com>
2021-05-29 13:40:53 +0900

    編集画面でgraphicの再レンダーを抑止

--- components/lesson/backgroundImage.js
+++ components/lesson/backgroundImage.js
@@ -3,16 +3,18 @@ import React from 'react'
 import Image from 'next/image'
 import { css } from '@emotion/core'
 
-export default function LessonBackgroundImage({ url }) {
+const BackgroundImage = React.memo(function backgroundImage({ url }) {
   return (
     <div css={bodyStyle} className="bg-image-z">
       {url && <Image src={url} width={1280} height={720} />}
     </div>
   )
-}
+})
 
 const bodyStyle = css({
   position: 'absolute',
   top: 0,
   fontSize: 0, // next/imageで出力されるトップレベルの要素がinline-blockなので余白をなくすために指定
 })
+
+export default BackgroundImage
--- components/lesson/edit/graphicThumbnail.js
+++ components/lesson/edit/graphicThumbnail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import Image from 'next/image'
 import Container from '../../container'
 import LoadingIndicator from '../../loadingIndicator'
@@ -6,7 +6,7 @@ import AbsoluteContainer from '../../absoluteContainer'
 import ColorFilter from '../../colorFilter'
 import { useImageViewerContext } from '../../../libs/contexts/imageViewerContext'
 
-export default function GraphicThumbnail({ url, isProcessing }) {
+export default function GraphicThumbnail({ url, isProcessing=false }) {
   const { setImage } = useImageViewerContext()
   const [isLoaded, setIsLoaded] = useState(false)
 
@@ -19,18 +19,15 @@ export default function GraphicThumbnail({ url, isProcessing }) {
     setImage({ url, widePer: 70 })
   }
 
-  return (
+  return useMemo(() => (
     <Container position='relative' width='185' height='100'>
-      {url &&
-        <Image src={url} width="175" height="100" objectFit="contain" draggable={false} data-image-url={url} onClick={handleClick} onLoad={handleLoad} />
-      }
-      {(!isLoaded || isProcessing) &&
-        <AbsoluteContainer top='0' left='0'>
-          <ColorFilter filter='contrast(20%)'>
-            <Container width='185' height='100'><LoadingIndicator size={50} /></Container>
-          </ColorFilter>
-        </AbsoluteContainer>
+      {url && <Image src={url} width="175" height="100" objectFit="contain" draggable={false} data-image-url={url} onClick={handleClick} onLoad={handleLoad} />}
+      {(!isLoaded || isProcessing) &&  <AbsoluteContainer top='0' left='0'>
+        <ColorFilter filter='contrast(20%)'>
+          <Container width='185' height='100'><LoadingIndicator size={50} /></Container>
+        </ColorFilter>
+      </AbsoluteContainer>
       }
     </Container>
-  )
+  ), [url, isLoaded, isProcessing]) // Context由来のre-renderを抑止するため、memo化して依存変数を制限する
 }
