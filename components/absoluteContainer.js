cc04af318cc4e840d75cfaa3ba398fad01f9c240
SuperDog <super.dog.less.than.human@gmail.com>
2021-07-01 14:06:09 +0900

    汎用絶対位置表示componentのUIを調整

--- components/absoluteContainer.js
+++ components/absoluteContainer.js
@@ -1,19 +1,20 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react'
 import { css } from '@emotion/core'
-import Container from './container'
 
 export default function AbsoluteContainer(props) {
-  const { top, left, right, bottom, ...containerProps } = props
+  const { top, left, right, bottom, children } = props
   const bodyStyle = css({
     position: 'absolute',
     top,
     left,
     right,
     bottom,
+    width: '100%',
+    height: '100%',
   })
 
   return (
-    <div css={bodyStyle}><Container {...containerProps}/></div>
+    <div css={bodyStyle}>{children}</div>
   )
 }
