f917ca2ec91719d54a9091218bb2a1d74f067762
SuperDog <super.dog.less.than.human@gmail.com>
2021-07-01 21:15:47 +0900

    画像のプレースホルダ用componentを追加

---
+++ components/noImage.js
@@ -0,0 +1,21 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react'
+import { css } from '@emotion/core'
+import PlainText from './plainText'
+
+export default function NoImage({ textSize, color, backgroundColor }) {
+  const bodyStyle = css({
+    backgroundColor,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  })
+
+  return (
+    <div css={bodyStyle}>
+      <PlainText size={textSize} lineHeight={textSize} color={color}>NO IMAGE</PlainText>
+    </div>
+  )
+}
