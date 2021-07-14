99675900881cb671c280fffe7db728d47fcfd6ff
SuperDog <super.dog.less.than.human@gmail.com>
2021-07-14 20:23:24 +0900

    Input type=numberのコンポーネントに文字数制限を追加

--- components/form/inputNumber.js
+++ components/form/inputNumber.js
@@ -3,14 +3,14 @@ import React from 'react'
 import { css } from '@emotion/core'
 
 const InputNumber = React.forwardRef(function inputNumber(props, ref) {
-  const { size, color, backgroundColor, borderColor, borderWidth, ...inputProps } = props
+  const { size, color, backgroundColor, borderColor, borderWidth, maxLength, ...inputProps } = props
   const bodyStyle = css({
     width: 'calc(100% - 2px)',
     height: '100%',
     fontSize: `${size}px`,
     lineHeight: `${size}px`,
     color,
-    backgroundColor,
+    backgroundColor: backgroundColor || 'inherit',
     borderColor,
     borderWidth,
     borderStyle: 'solid',
@@ -19,10 +19,30 @@ const InputNumber = React.forwardRef(function inputNumber(props, ref) {
     ':focus': {
       outline: 'none', // 入力中はカーソルが表示されるのでnoneを許容する
     },
+    '::-webkit-outer-spin-button': {
+      WebkitAppearance: 'none',
+    },
+    '::-webkit-inner-spin-button': {
+      WebkitAppearance: 'none',
+    },
+    MozAppearance: 'textfield',
   })
 
+  function handleChange(e) {
+    if (maxLength) {
+      const value = e.currentTarget.value
+      if (value.length >= maxLength) {
+        e.currentTarget.value = value.slice(0, 10)
+      }
+    }
+
+    if (inputProps.onChange) {
+      inputProps.onChange(e)
+    }
+  }
+
   return (
-    <input type="number" {...inputProps} ref={ref} css={bodyStyle} />
+    <input type="number" {...inputProps} onChange={handleChange} ref={ref} css={bodyStyle} />
   )
 })
 
