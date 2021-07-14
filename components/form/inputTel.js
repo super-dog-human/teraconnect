e053582bd11b3a93de8818983b0c88826c7a922f
SuperDog <super.dog.less.than.human@gmail.com>
2021-07-14 20:36:42 +0900

    Input type=telの汎用コンポーネントを追加

---
+++ components/form/inputTel.js
@@ -0,0 +1,39 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react'
+import { css } from '@emotion/core'
+
+const InputTel = React.forwardRef(function inputTel(props, ref) {
+  const { size, color, backgroundColor, borderColor, borderWidth, onlyNumeric, ...inputProps } = props
+  const bodyStyle = css({
+    width: 'calc(100% - 2px)',
+    height: '100%',
+    fontSize: `${size}px`,
+    lineHeight: `${size}px`,
+    color,
+    backgroundColor: backgroundColor || 'inherit',
+    borderColor,
+    borderWidth,
+    borderStyle: 'solid',
+    padding: '0px',
+    margin: '0px',
+    ':focus': {
+      outline: 'none', // 入力中はカーソルが表示されるのでnoneを許容する
+    },
+  })
+
+  function handleChange(e) {
+    if (onlyNumeric) {
+      e.currentTarget.value = e.currentTarget.value.replace(/[^0-9]/g, '')
+    }
+
+    if (inputProps.onChange) {
+      inputProps.onChange(e)
+    }
+  }
+
+  return (
+    <input type="tel" {...inputProps} onChange={handleChange} ref={ref} css={bodyStyle} />
+  )
+})
+
+export default InputTel
