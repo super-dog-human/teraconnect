be1f8ca83628e599ef6924dcd60603792f1a5f00
SuperDog <super.dog.less.than.human@gmail.com>
2021-07-21 13:01:55 +0900

    フェードイン/アウトの汎用コンポーネントで使用するライブラリを変更し、その他のライブラリも更新

--- components/transitionContainer.js
+++ components/transitionContainer.js
@@ -1,31 +1,24 @@
-/** @jsxImportSource @emotion/react */
-import React from 'react'
-import { css } from '@emotion/core'
-import { CSSTransitionGroup } from 'react-transition-group'
+import React, { useEffect, useState } from 'react'
+import { useTransition, animated } from 'react-spring'
 
-export default function TransitionContainer({ isShow, name, showMsTime, hideMsTime, children }) {
-  const bodyStyle = css({
-    [`.${name}-enter`]: {
-      opacity: 0.01,
-    },
-    [`.${name}-enter.${name}-enter-active`]: {
-      opacity: 1,
-      transition: `opacity ${showMsTime || 1000}ms ease-in`,
-    },
-    [`.${name}-leave`]: {
-      opacity: 1,
-    },
-    [`.${name}-leave.${name}-leave-active`]: {
-      opacity: 0.01,
-      transition: `opacity ${hideMsTime || 1000}ms ease-in`,
+export default function TransitionContainer({ isShow, duration, children }) {
+  const [isContainerShow, setIsContainerShow] = useState(false)
+  const transition = useTransition(isShow, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    expires: false,
+    config: { duration },
+    onRest: () => {
+      if (!isShow) setIsContainerShow(false)
     },
   })
 
-  return (
-    <div css={bodyStyle}>
-      <CSSTransitionGroup transitionName="other-setting" transitionEnterTimeout={showMsTime} transitionLeaveTimeout={hideMsTime}>
-        {isShow && children}
-      </CSSTransitionGroup>
-    </div>
+  useEffect(() => {
+    if (isShow) setIsContainerShow(true)
+  }, [isShow])
+
+  return transition(
+    (styles, item) => item && <animated.div style={styles}>{isContainerShow && children}</animated.div>
   )
 }
