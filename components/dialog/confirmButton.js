59da0209587822cbb1ed1d83f1341a196c01df3a
SuperDog <super.dog.less.than.human@gmail.com>
2021-07-27 23:17:28 +0900

    ダイアログの実行中ローディング表示の色を調整

--- components/dialog/confirmButton.js
+++ components/dialog/confirmButton.js
@@ -7,7 +7,7 @@ export default function ConfirmButton({ onClick, isProcessing, name }) {
   return (
     <Container width='100'>
       <LabelButton onClick={onClick} fontSize='15' backgroundColor='var(--dark-gray)' color='var(--soft-white)' disabled={isProcessing}>
-        {isProcessing ? <LoadingIndicator size='30' /> : name || '実行'}
+        {isProcessing ? <LoadingIndicator size='30' color='white' /> : name || '実行'}
       </LabelButton>
     </Container>
   )
