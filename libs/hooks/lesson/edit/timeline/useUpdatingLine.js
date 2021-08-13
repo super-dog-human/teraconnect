3062231639039d137f6ecaf3e0b6834dc50247cd
SuperDog <super.dog.less.than.human@gmail.com>
2021-08-14 05:56:37 +0900

    編集画面で自身の開始時間を後にずらす場合、影響を受ける後続行は変更前の開始時間に基づくよう変更

--- libs/hooks/lesson/edit/timeline/useUpdatingLine.js
+++ libs/hooks/lesson/edit/timeline/useUpdatingLine.js
@@ -2,22 +2,33 @@ export default function useUpdatingLine({ shiftElapsedTime, nextElapsedTimeByKin
   function updateLine({ kind, index, elapsedTime, newValue, changeAfterLineElapsedTime }) {
     const { materials, setter } = targetMaterial(kind)
     const currentValue = materials.filter(m => m.elapsedTime === elapsedTime)[index]
-    updateMaterial(setter, currentValue, newValue)
-
     const durationDiff = ['embedding', 'graphic', 'music'].includes(kind) ? 0 : newValue.durationSec - currentValue.durationSec
     const elapsedDiff = newValue.elapsedTime - currentValue.elapsedTime
 
-    if (durationDiff + elapsedDiff === 0) return
+    if (durationDiff + elapsedDiff === 0) {
+      updateMaterial(setter, currentValue, newValue)
+      return
+    }
 
     if (elapsedDiff !== 0 && changeAfterLineElapsedTime) {
-      shiftElapsedTime({ fromElapsedTime: newValue.elapsedTime, offsetTime: durationDiff + elapsedDiff })
-    } else if (durationDiff !== 0) {
-      const nextElapsedTime = nextElapsedTimeByKind(newValue.elapsedTime, kind)
-      const endTime = newValue.elapsedTime + newValue.durationSec
-      if (nextElapsedTime <= endTime) { // 後続の行との時間重複があれば解消する
-        shiftElapsedTime({ fromElapsedTime: newValue.elapsedTime, offsetTime: endTime - nextElapsedTime + 0.001 })
-      } else {
-        shiftElapsedTime({ fromElapsedTime: newValue.elapsedTime, offsetTime: durationDiff })
+      if (elapsedDiff > 0) { // 自身を後に移動する場合、先に元の後続行を更新してから自身を更新する
+        shiftElapsedTime({ fromElapsedTime: elapsedTime, offsetTime: durationDiff + elapsedDiff })
+        updateMaterial(setter, currentValue, newValue)
+      } else {               // 自身を前に移動する場合、自身を更新してから新しい後続行を更新する
+        updateMaterial(setter, currentValue, newValue)
+        shiftElapsedTime({ fromElapsedTime: newValue.elapsedTime, offsetTime: durationDiff + elapsedDiff })
+      }
+    } else {
+      updateMaterial(setter, currentValue, newValue)
+
+      if (durationDiff !== 0) {
+        const nextElapsedTime = nextElapsedTimeByKind(newValue.elapsedTime, kind)
+        const endTime = newValue.elapsedTime + newValue.durationSec
+        if (nextElapsedTime <= endTime) { // 所要時間更新後も後続の行との時間重複があれば解消する
+          shiftElapsedTime({ fromElapsedTime: newValue.elapsedTime, offsetTime: endTime - nextElapsedTime + 0.001 })
+        } else {
+          shiftElapsedTime({ fromElapsedTime: newValue.elapsedTime, offsetTime: durationDiff })
+        }
       }
     }
   }
