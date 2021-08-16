dcbe04ce21805739445b3aaf521d87dd3e08a07b
SuperDog <super.dog.less.than.human@gmail.com>
2021-08-17 05:41:12 +0900

    不要なロジックの削除

--- components/lesson/player/index.js
+++ components/lesson/player/index.js
@@ -7,15 +7,15 @@ import Graphic from '../graphic'
 import Controller from './controller'
 
 export default function LessonPlayer(props) {
-  const { isPreparing, durationSec, backgroundImageURL, avatars, graphics, drawings, speeches, drawingRef, startDrawing, inDrawing, endDrawing, ...controllerProps } = props
+  const { isPreparing, durationSec, backgroundImageURL, avatars, graphics, drawings, drawingRef, startDrawing, inDrawing, endDrawing, ...controllerProps } = props
 
   return (
     <Aspect16To9Container>
       {isPreparing && <div>声の準備中</div>}
       {backgroundImageURL && <BackgroundImage url={backgroundImageURL} />}
-      {avatars    && <Avatar />}
-      {drawings   && <Drawing drawingRef={drawingRef} startDrawing={startDrawing} inDrawing={inDrawing} endDrawing={endDrawing} zKind='drawing' />}
-      {graphics   && <Graphic />}
+      {avatars && <Avatar />}
+      {drawings && <Drawing drawingRef={drawingRef} startDrawing={startDrawing} inDrawing={inDrawing} endDrawing={endDrawing} zKind='drawing' />}
+      {graphics && <Graphic />}
       <Controller maxTime={parseFloat(durationSec.toFixed(2))} {...controllerProps} />
     </Aspect16To9Container>
   )
