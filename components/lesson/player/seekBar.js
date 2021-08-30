a7ca0b3fb1c4c8f8b6e16e00dcd06d3cbc33edc4
SuperDog <super.dog.less.than.human@gmail.com>
2021-08-31 04:47:12 +0900

    プレーヤーでシークができなくなったデグレを修正

--- components/lesson/player/seekBar.js
+++ components/lesson/player/seekBar.js
@@ -4,12 +4,12 @@ import ContainerSpacer from '../../containerSpacer'
 import ColorFilter from '../../colorFilter'
 import InputRange from '../../form/inputRange'
 
-export default function SeekBar({ invisible, playerElapsedTime, maxTime, onSeekChange, onSeekUp }) {
+export default function SeekBar({ isPlaying, invisible, playerElapsedTime, maxTime, onSeekChange, onSeekUp }) {
   return (
     <Container invisible={invisible}>
       <ContainerSpacer left='15' right='15'>
         <ColorFilter filter='drop-shadow(2px 2px 2px var(--dark-purple))'>
-          <InputRange key={playerElapsedTime} color='var(--soft-white)' defaultValue={playerElapsedTime} min='0' max={parseFloat((maxTime).toFixed(2))}
+          <InputRange key={isPlaying ? playerElapsedTime : null} color='var(--soft-white)' defaultValue={playerElapsedTime} min='0' max={parseFloat((maxTime).toFixed(2))}
             step='0.01' onChange={onSeekChange} onMouseUp={onSeekUp}/>
         </ColorFilter>
       </ContainerSpacer>
