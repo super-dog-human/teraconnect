530e3ae194351ac5a453a0fe29b3d018539cb086
SuperDog <super.dog.less.than.human@gmail.com>
2021-08-31 15:59:04 +0900

    軽微な修正

--- components/lesson/player/titleBar.js
+++ components/lesson/player/titleBar.js
@@ -32,7 +32,7 @@ const containerStyle = css({
 const bodyStyle = css({
   width: '100%',
   minHeight: '50px',
-  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  backgroundColor: 'rgba(0, 0, 0, 0.7)',
   display: 'flex',
   alignItems: 'center',
 })
