e52c75aeffc1f4696e03bbb71acf5803ce3a7d2f
SuperDog <super.dog.less.than.human@gmail.com>
2021-09-03 03:38:33 +0900

    編集画面のキャッシュリセットは常に実行可能にする

--- components/authoringHeader/index.js
+++ components/authoringHeader/index.js
@@ -36,7 +36,7 @@ export default function Header({ currentPage, showBadge, isUpdating, updateLesso
       labels: ['上書き保存', '変更の破棄'],
       actions: [updateLesson, discardLessonDraft],
       position: { fixed: true, x: targetRect.x, y: targetRect.y + targetRect.height },
-      disableMenuIndexes: showBadge ? [] : [0, 1],
+      disableMenuIndexes: showBadge ? [] : [0],
     })
   }
 
