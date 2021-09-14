08afaf23fa02e70171122852c524748dea72d2b7
SuperDog <super.dog.less.than.human@gmail.com>
2021-09-15 08:48:27 +0900

    ユーザーアイコン画像が存在しない際、SSRではプレースホルダ表示に失敗する挙動を修正

--- components/userIcon.js
+++ components/userIcon.js
@@ -1,20 +1,25 @@
 /** @jsxImportSource @emotion/react */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { css } from '@emotion/core'
 import NoImage from './noImage'
 
 const iconURL = process.env.NEXT_PUBLIC_GOOGLE_STORAGE_BUCKET_URL + '/user/'
 
 export default function UserIcon({ id, name }) {
+  const [url ,setURL] = useState('')
   const [hasError, setHasError] = useState(false)
 
   function handleError() {
     setHasError(true)
   }
 
+  useEffect(() => {
+    setURL(iconURL + id + '.png')
+  }, [id])
+
   return (
     <div css={bodyStyle}>
-      {!hasError && <img src={iconURL + id + '.png'} onError={handleError} alt={name} css={imageStyle} />}
+      {!hasError && <img src={url} onError={handleError} alt={name} css={imageStyle} />}
       {hasError && <NoImage textSize='10' color='var(--soft-white)' backgroundColor='gray' />}
     </div>
   )
