7ba57578dd98fd251ca86c7d5cd6de4658090330
SuperDog <super.dog.less.than.human@gmail.com>
2021-09-15 12:23:06 +0900

    授業再生画面で同じ埋め込み動画を複数回表示している際、サムネイルも複数回表示されてしまう挙動を修正

--- components/lesson/index/embeddings.js
+++ components/lesson/index/embeddings.js
@@ -11,42 +11,40 @@ import Icon from '../../icon'
 export default function Embeddings({ embeddings }) {
   return (
     <Flex flexWrap='wrap' gap='50px 30px'>
-      {embeddings.filter(e => e.action === 'show').map((embedding, i) => {
-        const iframeURL = embedding.serviceName === 'youtube' ?
-          `https://www.youtube.com/embed/${embedding.contentID}?autoplay=0` :
-          `https://www.geogebra.org/material/iframe/id/${embedding.contentID}`
-        const linkURL = embedding.serviceName === 'youtube' ?
-          `https://www.youtube.com/watch?v=${embedding.contentID}` :
-          `https://www.geogebra.org/m/${embedding.contentID}`
+      {Array.from(new Set(embeddings.filter(e => e.action === 'show' && e.serviceName === 'youtube').map(e => e.contentID))).map((contentID, i) => {
+        const iframeURL = `https://www.youtube.com/embed/${contentID}?autoplay=0`
+        const linkURL = 'https://www.youtube.com/watch?v=' + contentID
 
         return (
           <div css={iframeStyle} key={i}>
-            {embedding.serviceName === 'youtube' &&
-              <>
-                <iframe id="ytplayer" type="text/html" width="300" height="169" src={iframeURL} frameBorder="0" />
-                <a href={linkURL} target='_blank' rel="noreferrer">
-                  <Flex justifyContent='right'>
-                    <Container width='15' height='15'><Icon name='foreign-gray' /></Container>
-                    <Spacer width='8' />
-                    <PlainText size='14' color='gray'>YouTube</PlainText>
-                  </Flex>
-                </a>
-              </>
-            }
-            {embedding.serviceName === 'geogebra' &&
-              <>
-                <iframe scrolling="no" width="300px" height="169px" src={iframeURL} frameBorder="0" />
-                <AlignContainer textAlign='right'>
-                  <a href={linkURL} target='_blank' rel="noreferrer">
-                    <Flex justifyContent='right'>
-                      <Container width='15' height='15'><Icon name='foreign-gray' /></Container>
-                      <Spacer width='8' />
-                      <PlainText size='14' color='gray'>GeoGebra</PlainText>
-                    </Flex>
-                  </a>
-                </AlignContainer>
-              </>
-            }
+            <iframe id="ytplayer" type="text/html" width="300" height="169" src={iframeURL} frameBorder="0" />
+            <a href={linkURL} target='_blank' rel="noreferrer">
+              <Flex justifyContent='right'>
+                <Container width='15' height='15'><Icon name='foreign-gray' /></Container>
+                <Spacer width='8' />
+                <PlainText size='14' color='gray'>YouTube</PlainText>
+              </Flex>
+            </a>
+          </div>
+        )
+      })}
+
+      {Array.from(new Set(embeddings.filter(e => e.action === 'show' && e.serviceName === 'geogebra').map(e => e.contentID))).map((contentID, i) => {
+        const iframeURL = 'https://www.geogebra.org/material/iframe/id/' + contentID
+        const linkURL = 'https://www.geogebra.org/m/' + contentID
+
+        return (
+          <div css={iframeStyle} key={i}>
+            <iframe scrolling="no" width="300px" height="169px" src={iframeURL} frameBorder="0" />
+            <AlignContainer textAlign='right'>
+              <a href={linkURL} target='_blank' rel="noreferrer">
+                <Flex justifyContent='right'>
+                  <Container width='15' height='15'><Icon name='foreign-gray' /></Container>
+                  <Spacer width='8' />
+                  <PlainText size='14' color='gray'>GeoGebra</PlainText>
+                </Flex>
+              </a>
+            </AlignContainer>
           </div>
         )
       })}
