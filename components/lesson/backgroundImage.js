7051b6d5415d3d0cd61c86f52b99afd43a3c198b
SuperDog <super.dog.less.than.human@gmail.com>
2021-09-17 01:52:20 +0900

    読み込み時間の改善のため背景画像をnext/imageからimgタグに変更

--- components/lesson/backgroundImage.js
+++ components/lesson/backgroundImage.js
@@ -1,12 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react'
-import Image from 'next/image'
 import { css } from '@emotion/core'
 
 const BackgroundImage = React.memo(function backgroundImage({ url }) {
   return (
     <div css={bodyStyle} className="bg-image-z">
-      {url && <Image src={url} width={1280} height={720} alt='背景画像' />}
+      {url && <img src={url} css={imageStyle} alt='背景画像' />}
     </div>
   )
 })
@@ -15,6 +14,12 @@ const bodyStyle = css({
   position: 'absolute',
   top: 0,
   fontSize: 0, // next/imageで出力されるトップレベルの要素がinline-blockなので余白をなくすために指定
+})
+
+const imageStyle = css({
+  width: '1280',
+  height: '720',
+  objectFit: 'contain',
 })
 
 export default BackgroundImage
