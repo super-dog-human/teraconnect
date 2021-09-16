32675eba0196de403c2765ce5cfaf7061cef8084
SuperDog <super.dog.less.than.human@gmail.com>
2021-09-16 13:17:48 +0900

    ランディングページをトップページに移動

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,35 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react'
+import { css } from '@emotion/core'
 import Head from 'next/head'
 import Layout from '../components/layout'
+import AloneInLandscape from '../components/landing/aloneInLandscape'
+import CopyForStudent from '../components/landing/copyForStudent'
+import PickedOutLesson from '../components/landing/pickedOutLesson'
+import CopyForTeacher from '../components/landing/copyForTeacher'
+import { BrowserView } from 'react-device-detect'
 
-export default function Page() {
+const Page = () => (
+  <>
+    <Head>
+      <title>TERACONNECT</title>
+    </Head>
+    <Layout>
+      <div css={bodyStyle}>
+        <AloneInLandscape />
+        <BrowserView>
+          <CopyForStudent />
+        </BrowserView>
+        <PickedOutLesson />
+        <CopyForTeacher />
+      </div>
+    </Layout>
+  </>
+)
 
-  return (
-    <>
-      <Head>
-        <title>TERACONNECT</title>
-      </Head>
-      <Layout>
-        <div>index.</div>
-      </Layout>
-    </>
-  )
-}
+const bodyStyle = css({
+  width: '100%',
+  height: '100%',
+})
+
+export default Page
