22ff711f8e941cd0145c41a354d5da344a757955
SuperDog <super.dog.less.than.human@gmail.com>
2021-09-18 02:56:36 +0900

    APIリクエストhooksにuseCallbackを追加

--- libs/hooks/useFetch.js
+++ libs/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useCallback, useEffect } from 'react'
 import { fetch as isoFetch, fetchToken as isoFetchToken, fetchWithAuth as isoFetchWithAuth, post as isoPost } from '../fetch'
 import { createVoice as isoCreateVoice, createGraphics as isoCreateGraphics, createMusic as isoCreateMusic } from '../fetchResource'
 
@@ -14,35 +14,7 @@ class TimeoutError extends Error {
 export default function useFetch() {
   const abortsRef = useRef({})
 
-  function fetch(resource, option) {
-    return requestWithAbortAndTimeout(signal => isoFetch(resource, { ...option, signal }))
-  }
-
-  function fetchToken() {
-    return requestWithAbortAndTimeout(signal => isoFetchToken({ signal }))
-  }
-
-  function fetchWithAuth(resource, token) {
-    return requestWithAbortAndTimeout(signal => isoFetchWithAuth(resource, token, { signal }))
-  }
-
-  function post(resource, body, method='POST', header) {
-    return requestWithAbortAndTimeout(signal => isoPost(resource, body, method, header, { signal }))
-  }
-
-  function createVoice(elapsedTime, durationSec, lessonID) {
-    return requestWithAbortAndTimeout(signal => isoCreateVoice(elapsedTime, durationSec, lessonID, { signal }))
-  }
-
-  function createGraphics(lessonID, files) {
-    return requestWithAbortAndTimeout(signal => isoCreateGraphics(lessonID, files, { signal }))
-  }
-
-  function createMusic(fileName) {
-    return requestWithAbortAndTimeout(signal => isoCreateMusic(fileName, { signal }))
-  }
-
-  function requestWithAbortAndTimeout(request) {
+  const requestWithAbortAndTimeout = useCallback(request => {
     const abortController = new AbortController()
 
     return new Promise((resolve, reject) => {
@@ -63,7 +35,35 @@ export default function useFetch() {
         delete abortsRef.current[timeout]
       })
     })
-  }
+  }, [])
+
+  const fetch = useCallback((resource, option) => {
+    return requestWithAbortAndTimeout(signal => isoFetch(resource, { ...option, signal }))
+  }, [requestWithAbortAndTimeout])
+
+  const fetchToken = useCallback(() => {
+    return requestWithAbortAndTimeout(signal => isoFetchToken({ signal }))
+  }, [requestWithAbortAndTimeout])
+
+  const fetchWithAuth = useCallback((resource, token) => {
+    return requestWithAbortAndTimeout(signal => isoFetchWithAuth(resource, token, { signal }))
+  }, [requestWithAbortAndTimeout])
+
+  const post = useCallback((resource, body, method='POST', header) => {
+    return requestWithAbortAndTimeout(signal => isoPost(resource, body, method, header, { signal }))
+  }, [requestWithAbortAndTimeout])
+
+  const createVoice = useCallback((elapsedTime, durationSec, lessonID) => {
+    return requestWithAbortAndTimeout(signal => isoCreateVoice(elapsedTime, durationSec, lessonID, { signal }))
+  }, [requestWithAbortAndTimeout])
+
+  const createGraphics = useCallback((lessonID, files) => {
+    return requestWithAbortAndTimeout(signal => isoCreateGraphics(lessonID, files, { signal }))
+  }, [requestWithAbortAndTimeout])
+
+  const createMusic = useCallback(fileName => {
+    return requestWithAbortAndTimeout(signal => isoCreateMusic(fileName, { signal }))
+  }, [requestWithAbortAndTimeout])
 
   useEffect(() => {
     return () => {
