92b8b7bd49656674da59e1958b5de5555fac0cfa
SuperDog <super.dog.less.than.human@gmail.com>
2021-09-18 22:57:59 +0900

    公開設定画面の関連授業サムネイルの縦横比を修正

--- components/lesson/publishing/index.js
+++ components/lesson/publishing/index.js
@@ -160,7 +160,7 @@ export default function LessonPublishing({ lesson, material }) {
                       <AbsoluteContainer top='0' left='0'>
                         <Container width='170' height='96'>
                           {!relationLessonThumbnailURL.prev && <NoImage textSize='12' color='gray' backgroundColor='lightgray' />}
-                          {relationLessonThumbnailURL.prev && <Image src={relationLessonThumbnailURL.prev} width={1280} height={720} alt='前の授業サムネイル' />}
+                          {relationLessonThumbnailURL.prev && <Image src={relationLessonThumbnailURL.prev} width={170} height={96} objectFit="contain" alt='前の授業サムネイル' />}
                         </Container>
                       </AbsoluteContainer>
                     </Aspect16To9Container>
@@ -178,7 +178,7 @@ export default function LessonPublishing({ lesson, material }) {
                       <AbsoluteContainer top='0' left='0'>
                         <Container width='170' height='96'>
                           {!relationLessonThumbnailURL.next && <NoImage textSize='12' color='gray' backgroundColor='lightgray' />}
-                          {relationLessonThumbnailURL.next && <Image src={relationLessonThumbnailURL.next} width={1280} height={720} alt='次の授業サムネイル' />}
+                          {relationLessonThumbnailURL.next && <Image src={relationLessonThumbnailURL.next} width={170} height={96} objectFit="contain" alt='次の授業サムネイル' />}
                         </Container>
                       </AbsoluteContainer>
                     </Aspect16To9Container>
