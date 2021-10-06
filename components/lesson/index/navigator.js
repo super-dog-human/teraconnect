ce1c25e4e9ca4edd30165a23fc4a8526f1aaa7ac
SuperDog <super.dog.less.than.human@gmail.com>
2021-10-06 14:31:29 +0900

    授業再生画面の前・次の授業UIのレイアウト崩れを修正

--- components/lesson/index/navigator.js
+++ components/lesson/index/navigator.js
@@ -1,15 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import React, { useState, useEffect } from 'react'
 import { css } from '@emotion/core'
-import PageLink from '../../pageLink'
-import PlainText from '../../plainText'
-import Flex from '../../flex'
-import FlexItem from '../../flexItem'
-import Container from '../../container'
-import ContainerSpacer from '../../containerSpacer'
-import Spacer from '../../spacer'
-import AlignContainer from '../../alignContainer'
-import NoImage from '../../noImage'
+import NavigatorItem from './navigatorItem'
 
 const lessonImageURL = process.env.NEXT_PUBLIC_GOOGLE_STORAGE_BUCKET_URL + '/lesson/'
 
@@ -37,59 +29,13 @@ export default function Navigator({ isMobile, lesson }) {
     <>
       {lesson && (lesson?.prevLessonID !== 0 || lesson?.nextLessonID !== 0) &&
         <div css={containerStyle}>
-          {(lesson.prevLessonID !== 0) &&
-            <FlexItem column={isMobile || lesson.nextLessonID === 0 ? '1' : '2 - 50px'}>
-              <ContainerSpacer top={isMobile && lesson.nextLessonID !== 0  && '20'} left={isMobile && '20'} right={isMobile && '20'}>
-                <PageLink path={'/lessons/' + lesson.prevLessonID}>
-                  <Flex alignItems='start'>
-                    <FlexItem flexBasis='150px'>
-                      <Container width='150' height='84'>
-                        {!thumbnailErrors.prev && <img src={thumbnailURLs.prev} alt={lesson.prevLessonTitle} css={thumbnailStyle} onError={handleThumbnailError} />}
-                        {thumbnailErrors.prev && <NoImage textSize='13' color='var(--soft-white)' backgroundColor='gray' />}
-                      </Container>
-                    </FlexItem>
-                    <Spacer width='15'/>
-                    <div css={labelStyle}>
-                      <PlainText size='13' lineHeight='13' color='gray'>
-                        <AlignContainer verticalAlign='top'>
-                        前の授業
-                        </AlignContainer>
-                      </PlainText>
-                      <div css={lessonTitleStyle}>
-                        {lesson.prevLessonTitle}
-                      </div>
-                    </div>
-                  </Flex>
-                </PageLink>
-              </ContainerSpacer>
-            </FlexItem>
+          {lesson.prevLessonID !== 0 &&
+            <NavigatorItem isMobile={isMobile} isNext={false} lessonID={lesson.prevLessonID} otherLessonID={lesson.nextLessonID} title={lesson.prevLessonTitle}
+              thumbnailURL={thumbnailURLs.prev} thumbnailError={thumbnailErrors.prev} onThumbnailError={handleThumbnailError} />
           }
-          {(lesson.nextLessonID !== 0) &&
-            <FlexItem column={isMobile || lesson.prevLessonID === 0 ? '1' : '2 - 50px'}>
-              <ContainerSpacer bottom={isMobile && lesson.prevLessonID !== 0  && '20'} left={isMobile && '20'} right={isMobile && '20'}>
-                <PageLink path={'/lessons/' + lesson.nextLessonID}>
-                  <Flex alignItems='start'>
-                    <FlexItem flexBasis='150px'>
-                      <Container width='150' height='84'>
-                        {!thumbnailErrors.next && <img src={thumbnailURLs.next} alt={lesson.nextLessonTitle} css={thumbnailStyle} onError={handleThumbnailError} />}
-                        {thumbnailErrors.next && <NoImage textSize='13' color='var(--soft-white)' backgroundColor='gray' />}
-                      </Container>
-                    </FlexItem>
-                    <Spacer width='15'/>
-                    <div css={labelStyle}>
-                      <PlainText size='13' lineHeight='13' color='gray'>
-                        <AlignContainer verticalAlign='top'>
-                        次の授業
-                        </AlignContainer>
-                      </PlainText>
-                      <div css={lessonTitleStyle}>
-                        {lesson.nextLessonTitle}
-                      </div>
-                    </div>
-                  </Flex>
-                </PageLink>
-              </ContainerSpacer>
-            </FlexItem>
+          {lesson.nextLessonID !== 0 &&
+            <NavigatorItem isMobile={isMobile} isNext={true} lessonID={lesson.nextLessonID} otherLessonID={lesson.prevLessonID} title={lesson.nextLessonTitle}
+              thumbnailURL={thumbnailURLs.next} thumbnailError={thumbnailErrors.next} onThumbnailError={handleThumbnailError} />
           }
         </div>
       }
@@ -106,24 +52,4 @@ const containerStyle = css({
   alignItems: 'center',
   flexWrap:   'wrap',
   gap: '20px 40px',
-})
-
-const thumbnailStyle = css({
-  width: '100%',
-  height: '100%',
-  objectFit: 'contain',
-})
-
-const labelStyle = css({
-  width: '100%',
-  height: '84px',
-})
-
-const lessonTitleStyle = css({
-  height: '66px',
-  marginTop: '5px',
-  marginLeft: '5px',
-  fontSize: '15px',
-  color: 'gray',
-  overflow: 'hidden',
 })
---
+++ components/lesson/index/navigatorItem.js
@@ -0,0 +1,62 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react'
+import { css } from '@emotion/core'
+import PageLink from '../../pageLink'
+import PlainText from '../../plainText'
+import Flex from '../../flex'
+import FlexItem from '../../flexItem'
+import Container from '../../container'
+import ContainerSpacer from '../../containerSpacer'
+import Spacer from '../../spacer'
+import AlignContainer from '../../alignContainer'
+import NoImage from '../../noImage'
+
+const NavigatorItem = ({ isMobile, isNext, lessonID, otherLessonID, title, thumbnailURL, thumbnailError, onThumbnailError }) => (
+  <FlexItem column={isMobile ? '1' : otherLessonID === 0 ? '2' : '2 - 50px'}>
+    <ContainerSpacer top={isMobile && !isNext && otherLessonID !== 0 && '20'} bottom={isMobile && isNext && otherLessonID !== 0 && '20'} left={isMobile && '20'} right={isMobile && '20'}>
+      <PageLink path={'/lessons/' + lessonID}>
+        <Flex alignItems='start'>
+          <FlexItem flexBasis='150px'>
+            <Container width='150' height='84'>
+              {!thumbnailError && <img src={thumbnailURL} alt={title} css={thumbnailStyle} onError={onThumbnailError} />}
+              {thumbnailError && <NoImage textSize='13' color='var(--soft-white)' backgroundColor='gray' />}
+            </Container>
+          </FlexItem>
+          <Spacer width='15'/>
+          <div css={labelStyle}>
+            <PlainText size='13' lineHeight='13' color='gray'>
+              <AlignContainer verticalAlign='top'>
+                {isNext ? '次の授業' : '前の授業'}
+              </AlignContainer>
+            </PlainText>
+            <div css={lessonTitleStyle}>
+              {title}
+            </div>
+          </div>
+        </Flex>
+      </PageLink>
+    </ContainerSpacer>
+  </FlexItem>
+)
+
+export default NavigatorItem
+
+const thumbnailStyle = css({
+  width: '100%',
+  height: '100%',
+  objectFit: 'contain',
+})
+
+const labelStyle = css({
+  width: '100%',
+  height: '84px',
+})
+
+const lessonTitleStyle = css({
+  height: '66px',
+  marginTop: '5px',
+  marginLeft: '5px',
+  fontSize: '15px',
+  color: 'gray',
+  overflow: 'hidden',
+})
