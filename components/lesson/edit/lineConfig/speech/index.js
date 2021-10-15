419977b1cc7dd9b1aac7b7c4959034d60dfc4a18
SuperDog <super.dog.less.than.human@gmail.com>
2021-10-15 16:00:35 +0900

    編集画面の音声行設定パネルのタブをタッチデバイスに対応

--- components/lesson/edit/lineConfig/speech/index.js
+++ components/lesson/edit/lineConfig/speech/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tab, TabList, Tabs, TabPanel, resetIdCounter } from 'react-tabs'
 import TabListWithCloseButton from '../../../../tab/tabListWithCloseButton'
 import TabContainer from '../../../../tab/tabContainer'
@@ -15,18 +15,27 @@ import 'react-tabs/style/react-tabs.css'
 
 export default function Speech(props) {
   const { isProcessing, config, dispatchConfig, handleConfirm, handleCancel } = useSpeechConfig(props)
+  const [tabIndex, setTabIndex] = useState(0)
+
+  function handleTabTouch(e) {
+    setTabIndex(parseInt(e.currentTarget.dataset.index))
+  }
 
   resetIdCounter()
 
   return (
     <>
       <TabContainer minHeight='340'>
-        <Tabs forceRenderTabPanel={true}>
+        <Tabs forceRenderTabPanel={true} selectedIndex={tabIndex} onSelect={() => {}}>
           <DragHandler>
             <TabList>
               <TabListWithCloseButton onClose={handleCancel} color='var(--soft-white)' disabled={isProcessing}>
-                <Tab><AlignContainer textAlign='center'><PlainText size='14'>字幕・テロップ</PlainText></AlignContainer></Tab>
-                <Tab><AlignContainer textAlign='center'><PlainText size='14'>音声</PlainText></AlignContainer></Tab>
+                <Tab data-index={0} onClick={handleTabTouch} onTouchEnd={handleTabTouch}>
+                  <AlignContainer textAlign='center'><PlainText size='14'>字幕・テロップ</PlainText></AlignContainer>
+                </Tab>
+                <Tab data-index={1} onClick={handleTabTouch} onTouchEnd={handleTabTouch}>
+                  <AlignContainer textAlign='center'><PlainText size='14'>音声</PlainText></AlignContainer>
+                </Tab>
               </TabListWithCloseButton>
             </TabList>
           </DragHandler>
